2f626743e804fd9ca44281c6580ae260ebd9a068
Apelsin <appelsindrikken@gmail.com>
2018-10-09 15:52:46 -0700

    trying this...

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ekstrakto',
     version='0.1dev',
     packages=['ekstrakto'],
-    scripts=['bin/ek.py'],
+    scripts=['bin/ek'],
     license='MIT',
     long_description=open('README.md').read(),
 )
