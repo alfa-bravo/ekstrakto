42a35301ca4a3dad1e10558f0f16f3e45beb0396
Apelsin <appelsindrikken@gmail.com>
2018-10-08 19:43:17 -0700

    setup pie

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ekstrakto',
+    version='0.1dev',
+    packages=['ekstrakto'],
+    license='MIT',
+    long_description=open('README.txt').read(),
+)
