272051be31d2f628c8c5ee3c2434b81668eb0240
Apelsin <appelsindrikken@gmail.com>
2018-10-11 09:41:48 -0700

    fixed up requires versions

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version='0.1dev',
     packages=['ekstrakto'],
     install_requires=[
-        'Pillow',
-        'scipy',
-        'scikit-learn',
+        'Pillow==5.3.0',
+        'scipy==1.1.0',
+        'scikit-learn==0.20.0',
 
         # fixes an issue in scikit-learn
         # https://github.com/scikit-learn/scikit-learn/issues/12226
-        'cloudpickle'
+        'cloudpickle==0.5.6'
     ],
     entry_points = {
         'console_scripts': [
